9169c676b57e766982a77f5ae441f3968bff68c8
shufa <yshufan091@gmail.com>
2025-08-05 10:48:46 +0800

    0804

--- HW5-B11021001-楊書帆.py
+++ HW5-B11021001-楊書帆.py
@@ -16,17 +16,18 @@ learning_rate = 0.01                                                            
 k = 0                                                                                             # 計算第k次初始化為0
 Max_iteration = 50                                                                                # 最多計算50個週期
 c = 0                                                                                             # 目前計算第幾組
-updated = True
+CCount_Max = 0
 
 X = np.column_stack((np.ones(m), X1))                                                             # bias 項 = X0 = 1
 T = np.array(t)
 W = np.array([10.0, -1.0])                                                                        # 初始權重
 
 weight_history = []                                                                               # 記錄每個Iteration的權重變化
+W_best = []
 
 for k in range(Max_iteration):
     print(f"\n===================== iteration{k+1} =====================")
-    updated = False                                                                               #新訓練迴圈開始設為False，有誤分類發生且更新權重設為True
+    CCount = 0                                                                                    # 正確計算的數量
 
     # 逐筆資料檢查
     for i in range(m):
@@ -36,12 +37,28 @@ for k in range(Max_iteration):
 
         error = T[i] - y                                                                          # 計算誤差
 
-        for j in range(len(W)):                                                                   # 逐筆W去修正
-            W[j] += learning_rate * error * X[i][j]                                               # 更新權重
+        if error != 0:                                                                            # 如果誤差不等於0
+            for j in range(len(W)):                                                               # 逐筆W去修正
+                W[j] += learning_rate * error * X[i][j]                                           # 更新權重
+
+        else:                                                                                     # 誤差等於0
+            CCount += 1                                                                           # 正確計算數量+1
+
+        if CCount > CCount_Max:                                                                   # 正確數量 > 目前正確數量
+            CCount_Max = CCount                                                                   # 更新正確數量
+            W_best = W.copy()
+
+        k += 1
 
     weight_history.append(W.copy())                                                               # 記錄歷史權重
+    W_best.append(W.copy())                                                                       # 紀錄目前最佳權重
 
     print(f"Updated Weights: {W}")
+
+    if CCount == m:  # 若所有樣本皆正確分類，則結束訓練                                                # 如果正確分類次數 = 資料筆數
+        print("\nCCount=m， W_best:")
+        print(W_best)
+        break
 
 result = pd.DataFrame({                                                                           # 將最佳銓重結果儲存到result變數
     'Best_W1' : [W[0]],
